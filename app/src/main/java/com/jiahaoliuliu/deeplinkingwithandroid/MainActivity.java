65a01c2cbb2360721eba46758b8574d1de14bf73
Jiahao Liu <jiahao.liu@dubizzle.com>
2015-04-09 18:28:40 +0400

    Created deep linking detection in Main activity

--- app/src/main/java/com/jiahaoliuliu/deeplinkingwithandroid/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/deeplinkingwithandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.jiahaoliuliu.deeplinkingwithandroid;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,9 @@ import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String DEEP_LINKING_SCHEMA = "deeplinkingwithandroid";
+    private static final String DEEP_LINKING_HOST = "details";
 
     private Button startDetailsActivityButton;
 
@@ -26,11 +30,29 @@ public class MainActivity extends ActionBarActivity {
         startDetailsActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent startDetailsActivityIntent = new Intent(MainActivity.this, DetailsActivity.class);
-                startDetailsActivityIntent
-                        .putExtra(DetailsActivity.INTENT_SOURCE_KEY, getString(R.string.source_main_activity));
-                startActivity(startDetailsActivityIntent);
+                startDetailsActivity(getString(R.string.source_main_activity));
             }
         });
+    }
+
+    private void startDetailsActivity(String source) {
+        Intent startDetailsActivityIntent = new Intent(MainActivity.this, DetailsActivity.class);
+        startDetailsActivityIntent
+                .putExtra(DetailsActivity.INTENT_SOURCE_KEY, source);
+        startActivity(startDetailsActivityIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Check if the activity has been started by deep linking
+        Uri data = getIntent().getData();
+
+        if (data != null
+                && DEEP_LINKING_SCHEMA.equalsIgnoreCase(data.getScheme())
+                && DEEP_LINKING_HOST.equalsIgnoreCase(data.getHost())) {
+            startDetailsActivity(getString(R.string.source_deep_linking));
+        }
     }
 }
